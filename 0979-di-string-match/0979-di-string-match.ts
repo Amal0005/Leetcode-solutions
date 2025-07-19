88e1a283ce5993dad0a0b0de1bf66ef5ffb95e7a
Amal Nt <amalachu441345@gmail.com>
2025-07-19 20:21:37 +0530

    Time: 4 ms (32.56%), Space: 60.8 MB (25.58%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.ts
@@ -0,0 +1,18 @@
+function diStringMatch(s: string): number[] {
+    let small: number = 0;
+    let large: number = s.length;
+    let arr: number[] = [];
+
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "I") {
+            arr.push(small)
+            small++
+        } else {
+            arr.push(large)
+            large--
+        }
+    }
+    arr.push(small)
+    return arr
+};
