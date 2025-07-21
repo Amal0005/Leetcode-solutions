be1a0d9222b4bc1a51a81af1abd22527d3f9551e
Amal Nt <amalachu441345@gmail.com>
2025-07-21 21:39:28 +0530

    Time: 58 ms (69.79%), Space: 64.6 MB (89.58%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+let  res=s[0]
+let count=1
+for(i=1;i<s.length;i++){
+    if(s[i]==s[i-1]){
+        count++
+    }else{
+        count=1
+    }
+    if(count<3){
+        res+=s[i]
+    }
+}
+return res
+};
