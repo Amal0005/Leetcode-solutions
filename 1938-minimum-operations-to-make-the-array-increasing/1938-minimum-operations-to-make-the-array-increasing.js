67572b0cb2d2898aa1a5a61c3c94f098a7cc511e
Amal Nt <amalachu441345@gmail.com>
2025-08-09 12:19:12 +0530

    Time: 880 ms (5.07%), Space: 56.3 MB (15.94%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.js
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minOperations = function (nums) {
-    count = 0
-    for (let i = 0; i < nums.length; i++) {
-        for (j = i + 1; j < nums.length; j++) {
-            if (nums[i] >= nums[j]) {
-                nums[j] = nums[j] + 1
+var minOperations = function(nums) {
+    count=0
+    for(i=0;i<nums.length;i++){
+        for(j=i+1;j<nums.length;j++){
+            if(nums[i]>=nums[j]){
+                nums[j]=nums[j]+1
                 count++
                 j--
-            } else {
+            }else{
                 i++
             }
         }
     }
-    return count
+return count
 };
