a1dd02f457cd4ff20acb7bd9e09b09a1d4490920
Amal Nt <amalachu441345@gmail.com>
2025-08-19 21:32:29 +0530

    Time: 1 ms (72.41%), Space: 56.4 MB (32.76%) - LeetHub

---
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var sumOfGoodNumbers = function (nums, k) {
+    let sum = 0
+    for (i = 0; i < nums.length; i++) {
+        if ((i - k < 0 || nums[i] > nums[i - k]) &&
+            (i + k >= nums.length || nums[i] > nums[i + k])) {
+            sum += nums[i]
+        }
+    }
+    return sum
+};
