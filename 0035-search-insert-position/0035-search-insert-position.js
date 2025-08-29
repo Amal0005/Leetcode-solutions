94f6589f11893f3ed2996ac3a9abbbdbd71b734f
Amal Nt <amalachu441345@gmail.com>
2025-08-29 19:55:08 +0530

    Time: 0 ms (100%), Space: 53.5 MB (72.16%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    
+   let low = 0, high = nums.length;
+    while(low<high) { 
+        let mid=low+Math.floor((high-low)/2);
+        if (target>nums[mid]) {
+            low=mid+1
+        } else {
+            high=mid
+        }
+    }
+    return low;
+};
