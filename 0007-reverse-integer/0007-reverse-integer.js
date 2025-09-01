9d9f9feddb79450b8100a6a2df54293182e5ff09
Amal Nt <amalachu441345@gmail.com>
2025-09-01 21:04:00 +0530

    Time: 57 ms (22.19%), Space: 55.7 MB (37.99%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -2,26 +2,24 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    let arr=""
-    a=x.toString().split("");
-    for(let i=a.length-1;i>=0;i--){
-        if(a[i]!=="-"){
-            arr+=a[i]
+var reverse = function (x) {
+    let arr = ""
+    a = x.toString().split("");
+    for (let i = a.length - 1; i >= 0; i--) {
+        if (a[i] !== "-") {
+            arr += a[i]
         }
     }
     let b;
-// let b=x>0 ? Number(arr):-Number(arr)   
-if(x>0){
-    b=Number(arr)
-}else{
-    b=-Number(arr)
-}
-console.log(b)
+    if (x > 0) {
+        b = Number(arr)
+    } else {
+        b = -Number(arr)
+    }
+    console.log(b)
 
-if(b<-2147483648||b>2147483648)
-{
- return 0;
-}
-return b
+    if (b < -2147483648 || b > 2147483648) {
+        return 0;
+    }
+    return b
 };
