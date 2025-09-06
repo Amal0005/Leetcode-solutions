16c1592290ffa0224e3d955560c5f77698d3e5e4
Amal Nt <amalachu441345@gmail.com>
2025-09-06 20:22:30 +0530

    Time: 0 ms (100%), Space: 56.8 MB (73.72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,6 +11,24 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
- merge=list1.concat(list2);
- console.log(merge) 
+   if (!list1) return list2;
+   if (!list2) return list1;
+
+   let head = new ListNode(0); 
+   let current = head;
+
+   while (list1 && list2) {
+       if (list1.val < list2.val) { 
+           current.next = list1;
+           list1 = list1.next;
+       } else {
+           current.next = list2;
+           list2 = list2.next;
+       }
+       current = current.next;
+   }
+
+   current.next = list1 !== null ? list1 : list2; 
+
+   return head.next; 
 };
