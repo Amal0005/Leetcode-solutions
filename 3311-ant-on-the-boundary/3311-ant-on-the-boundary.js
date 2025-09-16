c70f36f97dbb36f26f03460f4ca25c8a09ee2109
Amal Nt <amalachu441345@gmail.com>
2025-09-16 16:27:03 +0530

    Time: 54 ms (20.4%), Space: 56.9 MB (6.97%) - LeetHub

---
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function (nums) {
+    let sum = 0
+    let count = 0
+    for (i = 0; i < nums.length; i++) {
+        sum+=nums[i]
+        if(sum==0){
+            count++
+        }
+    }
+    return count
+};
