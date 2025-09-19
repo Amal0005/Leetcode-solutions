eaac227d40345e6f8ee65c550d5cf203dd5841fd
Amal Nt <amalachu441345@gmail.com>
2025-09-19 16:47:24 +0530

    Time: 1 ms (55.91%), Space: 58.5 MB (20.43%) - LeetHub

--- 3221-find-the-peaks/3221-find-the-peaks.js
+++ 3221-find-the-peaks/3221-find-the-peaks.js
@@ -3,9 +3,10 @@
  * @return {number[]}
  */
 var findPeaks = function(mountain) {
-      let arr=[]
-    for(let i=0;i<mountain.length;i++){
-        if(mountain[i-1]<mountain[i]&&mountain[i]>mountain[i+1]){
+  
+    let arr=[]
+    for(i=0;i<mountain.length;i++){
+        if(mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]){
             arr.push(i)
         }
     }
