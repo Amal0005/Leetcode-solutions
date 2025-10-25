4c2cd66ae5c067dee5f28cd86526db07335bb967
Amal Nt <amalachu441345@gmail.com>
2025-10-25 09:21:09 +0530

    Time: 1 ms (18.48%), Space: 55.8 MB (14.13%) - LeetHub

--- 1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.js
@@ -2,20 +2,20 @@
  * @param {number} n
  * @return {string}
  */
-var generateTheString = function(n) {
-    let res=""
-    if(n%2!==0){
-        for(let i=0;i<n;i++){
-            res+="a"
-        }
-    }else{
-           for(let i=0;i<n;i++){
-            if(i==n-1){
-                res+="b"
+var generateTheString = function (n) {
+    let str = ""
+    for (let i = 0; i < n; i++) {
+        if (n % 2 !== 0) {
+            str += "a"
+        } else {
+
+            if (i == (n - 1)) {
+                str += "b"
                 break
+            } else {
+                str += "a"
             }
-            res+="a"
         }
     }
-    return res
+    return str
 };
