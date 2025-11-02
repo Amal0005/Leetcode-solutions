724e2da43111b59d393c08ab2194bc1c9eb19ca8
Amal Nt <amalachu441345@gmail.com>
2025-11-02 22:33:22 +0530

    Time: 0 ms (100%), Space: 54.2 MB (76.09%) - LeetHub

--- 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.js
+++ 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var dominantIndex = function(nums) {
-       let a=Math.max(...nums)
-            b=nums.indexOf(a)
-    for(i=0;i<nums.length;i++){
-        if((nums[i]*2)>a&&i!=b){
-         return -1
-          }
+var dominantIndex = function (nums) {
+    let a = Math.max(...nums)
+    b = nums.indexOf(a)
+    for (i = 0; i < nums.length; i++) {
+        if ((nums[i] * 2) > a && i != b) {
+            return -1
+        }
     }
     return b
 };
