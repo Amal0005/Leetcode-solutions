b92abb89b9be9e53ded2356e9772ee8121929d83
Amal Nt <amalachu441345@gmail.com>
2025-11-10 19:57:36 +0530

    Time: 1 ms (79.83%), Space: 56.6 MB (75.63%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function (nums, k, multiplier) {
+    for (let i = 0; i < k; i++) {
+        let min = Math.min(...nums);
+        let indx = nums.indexOf(min);
+        nums[indx] = min * multiplier;
+    }
+    return nums;
+};
