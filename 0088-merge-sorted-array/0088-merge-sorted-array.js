62392564ff95cfb1195db58f926bac0df1a80832
Amal Nt <amalachu441345@gmail.com>
2025-11-14 15:36:13 +0530

    Time: 0 ms (100%), Space: 55.4 MB (24.16%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    for(let i=0;i<nums1.length;i++){
+        if(nums1[i]==0){
+            nums1[i]=nums2[0]
+            nums2.shift()
+           
+        }
+    }
+    for(let i=0;i<nums1.length;i++){
+        if(nums1[i]==undefined){
+            nums1[i]=0
+        }
+    }
+   return nums1.sort((a,b)=>a-b)
+};
