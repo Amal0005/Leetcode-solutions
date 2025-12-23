4aa940e50322523acf2a566a62b2a4abac2989c3
Amal Nt <amalachu441345@gmail.com>
2025-12-23 20:41:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isPossibleToSplit = function (nums) {
+    let freq = nums.reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1;
+        return acc;
+    }, {});
+    const arr = Object.values(freq)
+    return !arr.every((item) => item > 2)
+};
