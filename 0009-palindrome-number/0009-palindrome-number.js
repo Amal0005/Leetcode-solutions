1fa89544e7ddda4347f63a0cb1d4039914fe011b
Amal Nt <amalachu441345@gmail.com>
2025-12-25 22:08:19 +0530

    Time: 8 ms (47.76%), Space: 65.6 MB (19.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,14 +2,12 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-// s=x.toString()
-// t=s.reverse()
+var isPalindrome = function (x) {
 
-b=x.toString().split("").reverse().join("")
-if(x==b){
-    return true
-}else{
-    return false
-}
+    b = x.toString().split("").reverse().join("")
+    if (x == b) {
+        return true
+    } else {
+        return false
+    }
 }
