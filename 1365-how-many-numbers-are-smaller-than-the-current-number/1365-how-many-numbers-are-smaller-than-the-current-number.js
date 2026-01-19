caa79207964b09f6d23b10f8b39b5d7659578b73
Amal Nt <amalachu441345@gmail.com>
2026-01-19 21:03:59 +0530

    Time: 12 ms (25.7%), Space: 56.2 MB (92.84%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -2,12 +2,12 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var smallerNumbersThanCurrent = function(nums) {
-       let result=[];
-    for (i=0;i<nums.length;i++){
-        let count=0;
-        for (j=0;j<nums.length;j++){
-            if (nums[j]<nums[i]){
+var smallerNumbersThanCurrent = function (nums) {
+    let result = [];
+    for (i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
                 count++;
             }
         }
