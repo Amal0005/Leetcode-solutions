1cb6d6988313ba541a7b0d0090384475fb632a29
Amal Nt <amalachu441345@gmail.com>
2026-01-21 20:11:11 +0530

    Time: 244 ms (12.46%), Space: 59 MB (65.41%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -2,23 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findErrorNums = function(nums) {
+var findErrorNums = function (nums) {
     let missing = -1;
     let n = nums.length;
-    for(let i=1;i<=nums.length;i++) {
-        if(!nums.includes(i)) {
+    for (let i = 1; i <= nums.length; i++) {
+        if (!nums.includes(i)) {
             missing = i;
             break;
         }
     }
-    let s = Math.floor((n*(n+1))/2);
+    let s = Math.floor((n * (n + 1)) / 2);
     let arrSum = 0;
-    for(let i=0;i<nums.length;i++) {
+    for (let i = 0; i < nums.length; i++) {
         arrSum += nums[i];
     }
 
     arrSum += missing;
 
     let duplicate = arrSum - s;
-    return [duplicate,missing];
+    return [duplicate, missing];
 };
