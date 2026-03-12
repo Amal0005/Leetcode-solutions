4459ffba103b6408c6ee45ae9af2afcb53c0133f
Amal Nt <amalachu441345@gmail.com>
2026-03-12 21:09:58 +0530

    Time: 4 ms (43.23%), Space: 57.2 MB (26.5%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -2,12 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var thirdMax = function(nums) {
-    let dist=[...new Set(nums)];
-    num=dist.sort((a,b)=>(b-a))
-    if(num.length>=3){
-    return dist[2]
-}else{
-    return dist[0]
-}
+var thirdMax = function (nums) {
+    let dist = [...new Set(nums)];
+    num = dist.sort((a, b) => (b - a))
+    if (num.length >= 3) {
+        return dist[2]
+    } else {
+        return dist[0]
+    }
 };
