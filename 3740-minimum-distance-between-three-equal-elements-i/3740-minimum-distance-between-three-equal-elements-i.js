5ce79117e8d3d0debdad1c33d95ea8e744dd3106
Amal Nt <amalachu441345@gmail.com>
2026-04-11 18:13:48 +0530

    Time: 6 ms (35%), Space: 56.2 MB (65%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDistance = function(nums) {
+    let min=Infinity
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            for(k=j+1;k<nums.length;k++){
+                if(nums[i] == nums[j]&&nums[j] == nums[k]){
+                    let a=Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i)
+                   min=Math.min(min,a)
+                }
+
+            }
+        }
+    }
+    return min === Infinity ? -1 : min;
+};
