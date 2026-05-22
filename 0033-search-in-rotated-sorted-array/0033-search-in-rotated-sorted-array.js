1c848b5d43ad18d2d54ca958b3421571657e01cf
Amal Nt <amalachu441345@gmail.com>
2026-05-22 11:48:55 +0530

    Time: 0 ms (100%), Space: 53.9 MB (56.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,12 +3,11 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    for(i=0;i<nums.length;i++){
-        if(target===nums[i]){
+var search = function (nums, target) {
+    for (i = 0; i < nums.length; i++) {
+        if (target === nums[i]) {
             return i
         }
-        
-        }
+    }
     return -1
 };
