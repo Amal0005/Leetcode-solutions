f88ab1bafd7f4a21a0195129952cff89fc6c210a
Amal Nt <amalachu441345@gmail.com>
2026-05-24 20:28:08 +0530

    Time: 40 ms (28.6%), Space: 69.4 MB (17.72%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    let obj = {};
+    for (let str of strs) {
+        let key = str.split("").sort().join("");
+        if (!obj[key]) {
+            obj[key] = [];
+        }
+        obj[key].push(str)
+    }
+    return Object.values(obj)
+};
