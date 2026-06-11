df740466d894672986828bc9cee1be64219f1e86
Amal Nt <amalachu441345@gmail.com>
2026-06-11 22:04:38 +0530

    Time: 0 ms (100%), Space: 54.7 MB (97.13%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -4,10 +4,5 @@
  */
 var addDigits = function (num) {
     if (num == 0) return num
-    if (num > 0 && num % 9 == 0) {
-        return 9
-    } else {
-        return num % 9
-    }
-
+    return num > 0 && num % 9 == 0 ? 9 : num % 9
 };
