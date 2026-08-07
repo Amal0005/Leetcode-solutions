8f05f6a651d698d87d9989e4c404f2b92dd70e68
Amal Nt <amalachu441345@gmail.com>
2026-08-07 23:52:01 +0530

    Time: 2 ms (52.65%), Space: 56 MB (77.88%) - LeetHub

---
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {character} x
+ * @param {character} y
+ * @return {string}
+ */
+var rearrangeString = function (s, x, y) {
+    let str1 = ""
+    let str2 = ""
+    for (let i = 0; i < s.length; i++) {
+        s[i] == x ? str2 += s[i] : str1 += s[i]
+    }
+    return str1 + str2
+};
